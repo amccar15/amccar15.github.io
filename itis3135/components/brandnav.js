028c07673f5fae4598179bc1333f49e658f79fe6
amccar15 <amccar15@uncc.edu>
2022-02-03 17:19:03 -0500

    Update brandnav.js

--- itis3135/components/brandnav.js
+++ itis3135/components/brandnav.js
@@ -1,15 +1,8 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction()
+function openNav()
 {
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
-    {
-        document.getElementById("navbar").style.padding="30px 10px";
-        document.getElementById("logo").style.padding="25px";
-    }
-    else
-    {
-        document.getElementsByID("navbar").style.padding="80px 10px";
-        document.getElementById("logo").style.padding="35px";
-    }
+    document.getElementById("navbar").style.width="100%";
+}
+function closeNav()
+{
+    document.getElementById("navbar").style.width="0%";
 }
