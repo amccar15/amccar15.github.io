9e4d1be887ad576fff9504b5b987285b7a522a9e
amccar15 <amccar15@uncc.edu>
2022-02-17 17:05:58 -0500

    Update polygons.js

--- itis3135/scripts/polygons.js
+++ itis3135/scripts/polygons.js
@@ -1,72 +1,77 @@
-function validation()
+object.onload = function(){display()};
+function display()
 {
-    var number = document.getElementById("num").value;
-
-    if(number < 1 || number > 10)
-    {
-        alert("This number is not between 1 and 10!");
-        return false;
-    }
-    return true;
+    var number = window.prompt("Hello! The Aquamarine Monkeys would love to get a number between 1 and 10! This will give you a polygon!");
+    roundNum(number);
 }
 
-function resetbtn()
+function validation(shape)
 {
-    document.getElementById('num').value = '';
+    if(shape < 1 || shape > 10)
+    {
+        shape = alert("Not between 1 and 10!");
+        return display(shape);
+    }
 }
 
-function getShape()
+function roundNum(number)
 {
-    var id = document.getElementById("num").value;
-    var message;
-    if(id == 1)
-    {
-        message = "You got a henagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 2)
-    {
-        message = "You got a digon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 3)
-    {
-        message = "You got a trigon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 4)
-    {
-        message = "You got a tetragon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 5)
-    {
-        message = "You got a pentagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 6)
-    {
-        message = "You got a hexagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 7)
-    {
-        message = "You got a heptagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 8)
-    {
-        message = "You got a octagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 9)
-    {
-        message = "You got a enneagon!"
-        document.getElementById("display").innerHTML = message;
-    }
-    if(id == 10)
-    {
-        message = "You got a hendecagon!"
-        document.getElementById("display").innerHTML = message;
-    }
+    let shape = Math.round(Math.abs(number));
+    validation(shape);
+    getShape(shape);
 }
+
+function getShape(number)
+{
+    do{
+        if(number == 1)
+        {
+            alert("You entered a: " + number +"\nYou got a henagon!");
+        }
+        if(number == 2)
+        {
+            alert("You entered a: " + number + "\nYou got a digon!");
+        }
+        if(number == 3)
+        {      
+            alert("You entered a: " + number + "\nYou got a trigon!");
+        }
+        if(number == 4)
+        {
+            alert("You entered a: " + number + "\nYou got a tetragon!");
+        }
+        if(number == 5)
+        {
+            alert("You entered a: " + number + "\nYou got a pentagon!");
+        }
+        if(number == 6)
+        {
+            alert("You entered a: " + number + "\nYou got a hexagon!");
+        }
+        if(number == 7)
+        {
+            alert("You entered a: " + number + "\nYou got a heptagon!");
+        }
+        if(number == 8)
+        {
+            alert("You entered a: " + number + "\nYou got a octagon!");
+        }
+        if(number == 9)
+        {
+            alert("You entered a: " + number + "\nYou got a enneagon!");
+        }
+        if(number == 10)
+        {   
+            alert("You entered a: " + number + "\nYou got a hendecagon!");
+        }
+        var yes = window.confirm("Click OK to enter another number");
+        if(yes == true) 
+        {
+            return display();
+        }
+        if(yes == false) 
+        {
+            break;
+        }
+    }while(yes == true);
+} 
