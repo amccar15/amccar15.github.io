51d872685d1ba53f38deca6ad7f177fd6a666087
amccar15 <amccar15@uncc.edu>
2022-02-25 21:24:42 -0500

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,4 +1,45 @@
-function getNumber()
+const calculator =
 {
-    var number
+    number: 0,
+    operator: null
+};
+
+function getNumber(value)
+{
+    calculator.number = value;
+    console.log(calculator.number);
+    return calculator.number;
+}
+function getOperator(value)
+{
+    calculator.operator = value;
+    return calculator.operator;
+}
+function allclear()
+{
+    document.getElementById('display').innerHTML = "";
+    document.getElementById('displayOP').innerHTML = "";
+    calculator.number = 0;
+    calculator.operator = null;
+}
+function display(displayNum)
+{
+    if(calculator.number != 0)
+    {
+        document.getElementById('display').innerHTML += displayNum;
+    }
+     else if(calculator.number == 0)
+    {
+        document.getElementById('display').innerHTML = displayNum;
+    }
+}
+
+display();
+
+function calculate()
+{
+    if(getOperator() == '+')
+    {
+        console.log('plus');
+    }
 }
