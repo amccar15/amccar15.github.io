1bae8afe1641157b297ea4fdf394b7cbe5d38302
amccar15 <amccar15@uncc.edu>
2022-04-01 00:55:38 -0400

    Midterm

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,8 +1,26 @@
-$(function()
+$(document).ready(() =>
 {
-    $('.slider-inner').slideshow({
-            interval: 3000,
-            width: 600,
-            height: 400
-        });
+    $('.next').on('click', () =>
+    {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if(nextImg.length)
+        {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+    $('.prev').on('click', () =>
+    {
+        var currentImg = $('.active');
+        var pervImg = currentImg.prev();
+
+        if(pervImg.length)
+        {
+            currentImg.removeClass('active').css('z-index', -10);
+            pervImg.addClass('active').css('z-index', 10);
+        }
+    });
 });
