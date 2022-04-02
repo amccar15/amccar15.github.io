23f6ace8b7dfb7454f3220e36582d051f5c6da4d
amccar15 <amccar15@uncc.edu>
2022-04-01 22:50:04 -0400

    Update activity10.js

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -2,14 +2,35 @@ $(document).ready(function() {
 
 
     // preload the image for each link
-
+    $("#image_list a").each(function ()
+    {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr("href");
+    });
     // set up the event handlers for each link
+    $("#image_list").click(function (evt) 
+    {
+        // get the image URL and caption for each image and animate the caption
+        var imageURL = $(this).attr("href");
 
-            // get the image URL and caption for each image and animate the caption
+        $("#image").attr("src", imageURL);
 
+        var caption = $(this).attr("title");
+
+        $("#caption").fadeOut(3000, function()
+        {
+            $("#caption").text(caption).fadeIn(3000);
+
+            $('#caption').animate({ fontSize: "2.0em"}, 2000);
+        });
+
+        $('#image').fadeOut(3000, function ()
+        {
+            $('#image').attr("src", imageURL).fadeIn(3000);
+        })
         // cancel the default action of each link
-
-
-// move the focus to the first link
-
+        evt.preventDefault();
+    });
+    // move the focus to the first link
+    $("li:first-child a").focus();
 }); // end ready
