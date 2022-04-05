26493a4d533ed7c6afb0de81d96c58df8034ff1e
amccar15 <amccar15@uncc.edu>
2022-04-05 11:38:26 -0400

    activity 10 touch up

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,36 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-
-    // preload the image for each link
-    $("#image_list a").each(function ()
-    {
-        var swappedImage = new Image();
-        swappedImage.src = $(this).attr("href");
+    // preload images
+    $("#image_list a").each(function () {
+            var galleryImage = new Image();
+            galleryImage.src = $(this).attr("href")
     });
-    // set up the event handlers for each link
-    $("#image_list").click(function (evt) 
-    {
-        // get the image URL and caption for each image and animate the caption
-        var imageURL = $(this).attr("href");
 
-        $("#image").attr("src", imageURL);
+    // set up event handlers for links
+    // swaps image and caption by fading every 1 second  
+    $("#image_list a").click(function (evt) {
 
-        var caption = $(this).attr("title");
+            // variables so I can just call the var inside the function to make it cleaner
+            var imageURL = $(this).attr("href");
+            var imageCaption = $(this).attr("title");
 
-        $("#caption").fadeOut(3000, function()
-        {
-            $("#caption").text(caption).fadeIn(3000);
+            // swap caption
+            $("#caption").fadeOut(1000, function () {
+                    $("#caption").text(imageCaption).fadeIn(1000);
+            });
 
-            $('#caption').animate({ fontSize: "2.0em"}, 2000);
-        });
+            // swap image
+            $("#image").fadeOut(1000, function () {
+                    $("#image").attr("src", imageURL).fadeIn(1000);
+            });
 
-        $('#image').fadeOut(3000, function ()
-        {
-            $('#image').attr("src", imageURL).fadeIn(3000);
-        })
-        // cancel the default action of each link
-        evt.preventDefault();
+            // cancel the default action of the link
+            evt.preventDefault();
+
     });
-    // move the focus to the first link
+
+    // move focus to first thumbnail
     $("li:first-child a").focus();
+
 }); // end ready
