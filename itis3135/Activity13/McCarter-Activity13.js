a0fe331480408e0909ef9cd3506917907490399b
amccar15 <amccar15@uncc.edu>
2022-04-11 22:24:32 -0400

    activity13+ header change

---
+++ itis3135/Activity13/McCarter-Activity13.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "team.json",
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#team").html("");
+            $.each(data, function()
+            {
+              $.each(this, function(key, value)
+                {
+                    $("#team").append(
+                    "Name: " + value.name + "<br>",
+                    "Title: " + value.title + "<br>",
+                    "Bio: " + value.bio + "<br><br>"
+                    );
+                });  
+            });
+        }
+    });
+});
