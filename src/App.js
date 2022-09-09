35687bc513e322e2340383893dcad93cd040b68e
Macuser <amccar15@uncc.edu>
2022-09-09 13:09:26 -0400

    publish fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route, Link, HashRouter} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 import ThePost from './pages/ThePost';
 import Home from "./pages/Home";
@@ -22,7 +22,7 @@ function App() {
   }
 
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
         <nav>
           <ul>
             <li>
@@ -45,7 +45,7 @@ function App() {
           <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
           <Route path='/thepost' element={<ThePost />}></Route>
         </Routes>
-    </HashRouter>
+    </Router>
   )
 }
 
