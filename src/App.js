6b86961d3b277c981cfc04e9fba96a879079effc
Macuser <amccar15@uncc.edu>
2022-09-13 22:50:22 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {HashRouter as Router, Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import {HashRouter as Router, Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 import ThePost from './pages/ThePost';
 import Home from "./pages/Home";
